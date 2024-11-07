88e00c0fe64d896dcac1ef2f7b1f7e22f84b77a5
daminellis <matheus.daminelli@gmail.com>
2024-11-07 13:53:33 -0300

    ini

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+from database.db import db
+from routes.index import default_routes
+
+class App:
+    def __init__(self):
+        self.app = Flask(__name__)
+        CORS(self.app)
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3301/backendn2' #mudar de acordo com sua porta e entrada
+        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        db.init_app(self.app)
+        default_routes(self.app)
+
+    def run(self):
+        self.app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == '__main__':
+    app = App()
+    app.run() 
---
+++ src/routes/index.py
@@ -0,0 +1,9 @@
+#  Criação das rotas de acesso a aplicação
+
+#  from routes.nomedarota import nome da definicao
+
+# EX: from routes.loginroutes import login
+
+def default_routes(app):
+    #nomde da definicao
+    #login(app)
